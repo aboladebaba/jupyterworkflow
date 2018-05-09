f035c00a8d10505ad8707436ce11ceb37b4d8712
Abolade <abolade.babawale@gmail.com>
2018-05-08 23:25:00 -0500

    Move data download into jupyterworkflow package

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,27 @@
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+
+
+FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
+    """Download Fremont Bike Data
+    
+    Parameters
+    __________
+    filename : string (optional) location to save the data
+    url : string (optional) web location to pull the data from
+    force_download : bool (optional) if True, force redownload
+
+    Returns
+    _______
+    data : pandas.DataFrame the Fremont bridge data
+ 
+    """
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, filename)
+    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data.columns = ['West', 'East']
+    data['Total'] = data['West'] + data['East']
+    return data
