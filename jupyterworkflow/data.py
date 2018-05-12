fa0846a382ff394100d5f0340e6a1ada6eb0d43d
Abolade <abolade.babawale@gmail.com>
2018-05-11 22:53:35 -0500

    Speed up date parsing and ignore scratch pads

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -1,6 +1,7 @@
 import os
 from urllib.request import urlretrieve
 import pandas as pd
+import numpy as np
 
 
 FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
@@ -21,7 +22,11 @@ def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=Fal
     """
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
-    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data = pd.read_csv('Fremont.csv', index_col='Date')
+    try:
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+        data.index = pd.to_datetime(data.index)
     data.columns = ['West', 'East']
     data['Total'] = data['West'] + data['East']
     return data
